64fcd7b9f70e086abc7d8366ef3ee1f9bf6986c4
Rana <sanchitrana128@gmail.com>
2024-03-05 18:52:33 +0530

    Assignment 3 : create header element

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { FaReact, FaUserCircle } from "react-icons/fa";
 
-//JSX
-//JSX ==(babel)==> React.createElement ==(react)==> ReactElement - JS object => HTML Element (render)
-// const jsxHeading = <h1 id="heading">
-//     asjbsakasnj
-//     </h1>;
+//Header
 
-//Functional Component
-const Title = () => {
-  return <h1 id="heading">Title</h1>;
-};
+const Header=()=>{
+    return (
+        <div id="header" className="header">
+            <FaReact id="logo" size={30}/>
+            <input id="search"></input>
+            <FaUserCircle id="user" size={30}/>
+        </div>
+    )
+}
 
-const subHeading = <h2 id="heading">Sub Heading</h2>;
-
-//Functional Component
-const JsxHeading = () => {
-  return (
-    <div id="container">
-      <Title /> 
-      {subHeading}
-      <h3 id="heading">asjbsakasnj</h3>
-    </div>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById("root")).render(<JsxHeading />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Header />);
