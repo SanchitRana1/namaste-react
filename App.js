8cd8a8cf749acc93eca58d6dd64b72ea3f07bd75
Rana <sanchitrana128@gmail.com>
2024-03-08 15:27:12 +0530

    create header, restuarant card layout and data binding using props. Destructuring props.

--- App.js
+++ App.js
@@ -1,17 +1,81 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { FaReact, FaUserCircle } from "react-icons/fa";
+import logo from "./isolated_hamburger_with_splash_ink_background.jpg";
 
 //Header
+// -logo
+// -Nav Items
+//Body
+//-Search
+//  -RestaurantContainer
+//  -RestaurantCard : img , name, star rating, cuisine,
 
-const Header=()=>{
-    return (
-        <div id="header" className="header">
-            <FaReact id="logo" size={30}/>
-            <input id="search"></input>
-            <FaUserCircle id="user" size={30}/>
-        </div>
-    )
-}
+//Footer
+// -Copyright
+// -Links
+// -ContactInfo
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Header />);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo}></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestruantCard = ({resName,resCuisine,resInfo}) => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://www.solopress.com/blog/wp-content/uploads/2014/09/shutterstock_334960748.jpg"
+      ></img>
+      <h3 className="res-name">{resName}</h3>
+      <h4 className="res-cuisine">{resCuisine}</h4>
+      <div className="res-info">{resInfo}</div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestruantCard
+          resName="Durga Food"
+          resCuisine="North Indian, South Indian"
+          resInfo="4.0 stars"
+        />
+        <RestruantCard
+          resName="Lha Kitchen"
+          resCuisine="Koream, chinese"
+          resInfo="4.5 stars"
+        />
+      </div>
+    </div>
+  );
+};
+
+const AppComponent = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body></Body>
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<AppComponent />);
